2f4bce39399e79b1076301bd7e587c2a7d7dbdb4
Erika A Porath <55559514+TheErry@users.noreply.github.com>
2025-05-13 17:23:25 +0200

    Uploaded files

---
+++ SimpleMethod2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMethod2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
